e0408fb5d105c920e6e5e4ed3df7bf52d4c6a2e4
gash94 <104354207+gash94@users.noreply.github.com>
2023-07-14 21:34:32 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import sprite from './assets/icons/icons.svg';
-
+import logo from './assets/icons/big-logo.svg';
 function App() {
   return (
     <>
       <h1>WalletAPP</h1>
-      <svg width={400} height={400}>
-        <use href={sprite + '#icon__big-logo'}></use>
+      <svg width={400} height={400} fill="red">
+        <use href={sprite + '#icon__logo'}></use>
       </svg>
+      <img alt="logo" src={logo} />
     </>
   );
 }
