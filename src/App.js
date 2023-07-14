d9f4df015f3b2716e6b5644b01b649c570f5f581
gash94 <104354207+gash94@users.noreply.github.com>
2023-07-14 18:01:35 +0200

    add test svg

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import sprite from './assets/icons/icons.svg';
+
 function App() {
-  return <div>Just Some Random Test Text</div>;
+  return (
+    <>
+      <h1>WalletAPP</h1>
+      <svg width={400} height={400}>
+        <use href={sprite + '#icon__big-logo'}></use>
+      </svg>
+    </>
+  );
 }
 
 export default App;
