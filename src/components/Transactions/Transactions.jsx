00eaf70202d066676ddb7b54cb46afd0f4f359b3
Dominik Danielewicz <d.danielewicz00@gmail.com>
2023-07-17 20:21:23 +0200

    More styles

--- src/components/Transactions/Transactions.jsx
+++ src/components/Transactions/Transactions.jsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import { Icon } from 'components/Icon/Icon';
+import { PrimaryButton } from 'components/Buttons/Buttons';
 
 // padding - top;
 // padding - right;
@@ -10,21 +12,31 @@ const TransactionsTable = styled.table`
   border-spacing: 0;
   border: none;
   width: 100%;
+
+  color: #000;
+  font-family: Circe;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
 `;
 
-const TransactionTableHead = styled.thead``;
+const TransactionTableHead = styled.thead`
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
 
 const TransactionsTableHeadRow = styled.tr`
   background-color: #ffa000;
-  color: #ffffff;
   padding: 30px;
 `;
 
 const TransactionsTableHeader = styled.th`
-  padding: 30px;
+  padding: 15px 0px 15px 30px;
   text-align: left;
   background-color: #cfad70;
-  color: #fff;
 
   &:first-child {
     border-top-left-radius: 40px;
@@ -40,9 +52,37 @@ const TransactionsTableHeader = styled.th`
 const TransactionTableBody = styled.tbody``;
 
 const TransactionTableData = styled.td`
-  padding: 20px 0px 20px 30px;
+  padding: 15px 0px 15px 30px;
   border: none;
   border-bottom: solid 1px #957030;
+  box-shadow: 0px 1px 0px rgba(255, 255, 255);
+`;
+
+const EditButton = styled.button`
+  position: relative;
+  border: none;
+  background: none;
+  width: 14px;
+  height: 14px;
+  cursor: pointer;
+  vertical-align: middle;
+  margin-right: 8px;
+
+  & svg {
+    fill: none;
+    stroke: black;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+`;
+
+const SmallButton = styled(PrimaryButton)`
+  width: 67px;
+  height: 26px;
+  color: #fff;
+  font-size: 14px;
 `;
 
 const TransactionsBodyHeadRow = styled.tr``;
@@ -67,7 +107,12 @@ const Transactions = () => {
           <TransactionTableData>Other</TransactionTableData>
           <TransactionTableData>Gift for your wife</TransactionTableData>
           <TransactionTableData>300.00</TransactionTableData>
-          <TransactionTableData>ic Delete</TransactionTableData>
+          <TransactionTableData>
+            <EditButton>
+              <Icon class="svg" icon="icon__edit" width="14px" height="14px" />
+            </EditButton>{' '}
+            <SmallButton>Delete</SmallButton>
+          </TransactionTableData>
         </TransactionsBodyHeadRow>
         <TransactionsBodyHeadRow>
           <TransactionTableData>05.01.19</TransactionTableData>
@@ -75,7 +120,12 @@ const Transactions = () => {
           <TransactionTableData>Income</TransactionTableData>
           <TransactionTableData>January bonus</TransactionTableData>
           <TransactionTableData>8 000.00</TransactionTableData>
-          <TransactionTableData>ic Delete</TransactionTableData>
+          <TransactionTableData>
+            <EditButton>
+              <Icon className="svg" icon="icon__edit" width="14px" height="14px" />
+            </EditButton>{' '}
+            Delete
+          </TransactionTableData>
         </TransactionsBodyHeadRow>
       </TransactionTableBody>
     </TransactionsTable>
