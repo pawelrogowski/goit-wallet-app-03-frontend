cc89bdf41add5ad099c4ca1c31c26f067814b83d
gash94 <104354207+gash94@users.noreply.github.com>
2023-07-17 17:41:51 +0200

    test data.js

---
+++ src/components/DiagramTable/data.js
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    category: 'Main expenses',
+    sum: '8 700.00',
+    color: '#FED057',
+  },
+  {
+    category: 'Products',
+    sum: '3 800.74',
+    color: '#FFD8D0',
+  },
+  {
+    category: 'Car',
+    sum: '1 500.00',
+    color: '#FD9498',
+  },
+  {
+    category: 'Car',
+    sum: '1 500.00',
+    color: '#FD9498',
+  },
+  {
+    category: 'Car',
+    sum: '1 500.00',
+    color: '#FD9498',
+  },
+  {
+    category: 'Car',
+    sum: '1 500.00',
+    color: '#FD9498',
+  },
+];
