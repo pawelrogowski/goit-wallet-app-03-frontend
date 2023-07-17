c94e57aa7c36e47690f25f950010fc80551dd24e
gash94 <104354207+gash94@users.noreply.github.com>
2023-07-17 17:58:22 +0200

    Update data.js

--- src/components/DiagramTable/data.js
+++ src/components/DiagramTable/data.js
@@ -15,18 +15,33 @@ export const data = [
     color: '#FD9498',
   },
   {
-    category: 'Car',
-    sum: '1 500.00',
-    color: '#FD9498',
+    category: 'Self care',
+    sum: '800.00',
+    color: '#C5BAFF',
   },
   {
-    category: 'Car',
-    sum: '1 500.00',
-    color: '#FD9498',
+    category: 'Child care',
+    sum: '2208.00',
+    color: '#6E78E8',
   },
   {
-    category: 'Car',
-    sum: '1 500.00',
-    color: '#FD9498',
+    category: 'Household products',
+    sum: '300.00',
+    color: '#4A56E2',
+  },
+  {
+    category: 'Education',
+    sum: '3 400.00',
+    color: '#81E1FF',
+  },
+  {
+    category: 'Leisure',
+    sum: '1 230.00',
+    color: '#24CCA7',
+  },
+  {
+    category: 'Other expenses',
+    sum: '610.00',
+    color: '#00AD84',
   },
 ];
