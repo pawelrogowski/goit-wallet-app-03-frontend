9d9486a45bb70283bf49acba49fc30cf21a1fab6
Dawid <109389692+DawidBartus@users.noreply.github.com>
2023-07-18 21:15:55 +0200

    cleaning

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import { Container } from 'components/Container/Container';
 import DiagramTable from 'components/DiagramTable/DiagramTable';
 import { data } from 'components/DiagramTable/data';
 import Logo from 'components/Logo/Logo';
-import Currency from 'components/Currency/Currency';
-import Loader from 'components/Loader/Loader';
-import Balance from 'components/Balance/Balance';
 
 function App() {
   return (
@@ -15,11 +12,7 @@ function App() {
         <Logo></Logo>
         <LoginForm />
         <DiagramTable data={data} />
-        <Currency />
-        <Loader />
-        <Balance />
       </Container>
-      <br />
       <h1>WalletAPP</h1>
       <svg width={400} height={400}>
         <use href={sprite + '#icon__big-logo-man1'}></use>
