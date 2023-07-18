f373dd7cebe9a8b49ecbf54e58e283597ef0b271
Feiron <pawel.rogowski.praca@gmail.com>
2023-07-18 18:26:09 +0200

    added basic background with icons

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
-import sprite from './assets/icons/icons.svg';
 import LoginForm from 'components/LoginForm/LoginForm';
 import { Container } from 'components/Container/Container';
 import DiagramTable from 'components/DiagramTable/DiagramTable';
 import { data } from 'components/DiagramTable/data';
+import { Background } from 'components/Background/Background';
 
 function App() {
   return (
     <>
+      <Background />
       <Container>
         <LoginForm />
         <DiagramTable data={data} />
       </Container>
-      <h1>WalletAPP</h1>
-      <svg width={400} height={400}>
-        <use href={sprite + '#icon__big-logo-man1'}></use>
-      </svg>
-      <svg width={400} height={400}>
-        <use href={sprite + '#icon__big-logo-man2'}></use>
-      </svg>
-      <svg width={400} height={400}>
-        <use href={sprite + '#icon__big-logo-woman1'}></use>
-      </svg>
-      <svg width={400} height={400}>
-        <use href={sprite + '#icon__big-logo-woman2'}></use>
-      </svg>
-      <svg width={400} height={400}>
-        <use href={sprite + '#icon__logo'}></use>
-      </svg>
-      WalletAPP
     </>
   );
 }
