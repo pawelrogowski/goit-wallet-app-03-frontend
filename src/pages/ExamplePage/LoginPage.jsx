ee05d157fa00a0f14163e4615a391a184d28f3b6
Feiron <pawel.rogowski.praca@gmail.com>
2023-07-19 15:53:34 +0200

    bugfix/removed redundant import

--- src/pages/ExamplePage/LoginPage.jsx
+++ src/pages/ExamplePage/LoginPage.jsx
@@ -1,5 +1,4 @@
 import LoginForm from 'components/LoginForm/LoginForm';
-import { Background } from 'components/Background/Background';
 import { Icon } from 'components/Icon/Icon';
 import styled from 'styled-components';
 
