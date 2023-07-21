d915550a0c095aeb9ba2869c88635981550afe04
gash94 <104354207+gash94@users.noreply.github.com>
2023-07-21 18:17:35 +0200

    Update Layout.jsx

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,17 +1,31 @@
-import Header from 'components/Header/Header';
-import Navigation from 'components/Navigation/Navigation';
+import styled from 'styled-components';
 import { Suspense } from 'react';
-
 import { Outlet } from 'react-router-dom';
+import Header from 'components/Header/Header';
+import AsideBar from 'components/AsideBar/AsideBar';
 
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 0 20px;
+  background: rgba(255, 255, 255, 0.4);
+  backdrop-filter: blur(25px);
+  @media (min-width: ${props => props.theme.breakpoints.desktop}) {
+    flex-direction: row;
+    padding: 0 16px;
+  }
+`;
 const Layout = () => {
   return (
     <>
       <Header />
-      <Navigation />
-      <Suspense fallback={null}>
-        <Outlet />
-      </Suspense>
+      <Section>
+        <AsideBar />
+        <Suspense fallback={null}>
+          <Outlet />
+        </Suspense>
+      </Section>
     </>
   );
 };
