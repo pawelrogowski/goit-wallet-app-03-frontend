a244210d5f22708a6c92b4766b2d09112f7b7842
gash94 <104354207+gash94@users.noreply.github.com>
2023-07-21 18:17:41 +0200

    Update StatisticPage.jsx

--- src/pages/StatisticPage.jsx
+++ src/pages/StatisticPage.jsx
@@ -1,56 +1,43 @@
-import Balance from 'components/Balance/Balance';
-import Chart from 'components/Chart/Chart';
-import { Container } from 'components/Container/Container';
-import Currency from 'components/Currency/Currency';
-import DiagramTable from 'components/DiagramTable/DiagramTable';
-import Header from 'components/Header/Header';
-import { Heading } from 'components/Heading/Heading';
-import Navigation from 'components/Navigation/Navigation';
 import styled from 'styled-components';
+import Chart from 'components/Chart/Chart';
+import DiagramTable from 'components/DiagramTable/DiagramTable';
+import { Heading } from 'components/Heading/Heading';
 
-const Section = styled(Container)`
+const MainContainer = styled.div`
   display: flex;
   justify-content: space-between;
-`;
-const AsideContainer = styled.div`
-  flex-grow: 2;
-`;
-const MainContainer = styled.div`
-  margin-left: 70px;
-  padding-left: 70px;
-  border-left: 1px solid #dcdcdf;
-  box-shadow: 0px 1px 0px rgba(255, 255, 255, 0.6);
-  display: flex;
   flex-grow: 1;
+  flex-wrap: wrap;
 
-  & > :first-child {
-    flex-direction: column;
-    flex-wrap: wrap;
+  & > :nth-child(1n + 1) {
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 32px;
+    @media (min-width: ${props => props.theme.breakpoints.desktop}) {
+      margin-bottom: 20px;
+    }
+  }
+  @media (min-width: ${props => props.theme.breakpoints.desktop}) {
+    margin-left: 70px;
+    padding-left: 70px;
+    border-left: 1px solid #dcdcdf;
+    box-shadow: 0px 1px 0px rgba(255, 255, 255, 0.6);
   }
 `;
 const Title = styled(Heading)`
-  display: flex;
-  flex-direction: column;
+  display: inline-block;
+  padding: 0;
+  margin-bottom: 0;
+  width: 100%; // this 100% width is so the heading takes full width of a parent, pushing other elements below
 `;
 
 const StatisticPage = () => {
   return (
-    <>
-      <Header />
-      <Section as="section">
-        <AsideContainer as="aside">
-          <Navigation />
-          <Balance />
-          <Currency />
-        </AsideContainer>
-
-        <MainContainer as="main">
-          <Title as="h1">Statistic</Title>
-          <Chart />
-          <DiagramTable />
-        </MainContainer>
-      </Section>
-    </>
+    <MainContainer as="main">
+      <Title as="h1">Statistic</Title>
+      <Chart />
+      <DiagramTable />
+    </MainContainer>
   );
 };
 
