672aa71fd64d42ae407c8ae6b5dc43bccb7a52d9
gash94 <104354207+gash94@users.noreply.github.com>
2023-07-21 20:10:35 +0200

    Update StatisticPage.jsx

--- src/pages/StatisticPage.jsx
+++ src/pages/StatisticPage.jsx
@@ -3,27 +3,6 @@ import Chart from 'components/Chart/Chart';
 import DiagramTable from 'components/DiagramTable/DiagramTable';
 import { Heading } from 'components/Heading/Heading';
 
-const MainContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  flex-grow: 1;
-  flex-wrap: wrap;
-
-  & > :nth-child(1n + 1) {
-    margin-left: auto;
-    margin-right: auto;
-    margin-bottom: 32px;
-    @media (min-width: ${props => props.theme.breakpoints.desktop}) {
-      margin-bottom: 20px;
-    }
-  }
-  @media (min-width: ${props => props.theme.breakpoints.desktop}) {
-    margin-left: 70px;
-    padding-left: 70px;
-    border-left: 1px solid #dcdcdf;
-    box-shadow: 0px 1px 0px rgba(255, 255, 255, 0.6);
-  }
-`;
 const Title = styled(Heading)`
   display: inline-block;
   padding: 0;
@@ -33,11 +12,11 @@ const Title = styled(Heading)`
 
 const StatisticPage = () => {
   return (
-    <MainContainer as="main">
+    <>
       <Title as="h1">Statistic</Title>
       <Chart />
       <DiagramTable />
-    </MainContainer>
+    </>
   );
 };
 
