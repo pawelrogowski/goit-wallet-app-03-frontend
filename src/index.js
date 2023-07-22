666f998c69239234af49afafa0556a6f82fe196d
Feiron <pawel.rogowski.praca@gmail.com>
2023-07-22 02:04:37 +0200

    reordered imports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import 'modern-normalize/modern-normalize.css';
 import './stylesheet/fonts.css';
 import './stylesheet/vars.css';
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import { theme } from './stylesheet/theme';
 import { ThemeProvider } from 'styled-components';
