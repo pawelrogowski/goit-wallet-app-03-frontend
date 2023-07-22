21cdd49c92d1b9d7c06da053693ed9761f55f1a6
Feiron <pawel.rogowski.praca@gmail.com>
2023-07-22 19:24:49 +0200

    refactored the component, fixed icons, added css vars

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,30 +1,124 @@
+import { Icon } from 'components/Icon/Icon';
 import React from 'react';
-import NavigationElement from 'components/NavigationElement.jsx/NavigationElement';
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 
-const StyledDiv = styled.nav`
+const NavContainer = styled.nav`
   margin: 0;
-  display: flex;
-  gap: 30px;
-  justify-content: center;
-  align-items: center;
-  padding-top: 12px;
-  @media (min-width: ${props => props.theme.breakpoints.tablet}) {
-    display: flex;
-    flex-direction: column;
-    gap: 0px;
-    align-items: flex-start;
+  padding: 0;
+  display: inline-block;
+
+  ${Icon} {
+    fill: var(--nav-color-inactive);
+    width: 44px;
+    height: 44px;
+    position: relative;
+    /* border radius added so the box shadow works properly */
+    transition: fill 150ms, box-shadow 150ms;
+    border-radius: 5px;
+    @media (min-width: ${props => props.theme.breakpoints.tablet}) {
+      width: 23px;
+      height: 23px;
+      border-radius: 2px;
+    }
   }
 `;
 
-const Navigation = () => {
+const NavList = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  gap: 30px;
+
+  @media (min-width: ${props => props.theme.breakpoints.tablet}) {
+    flex-direction: column;
+    gap: 15px;
+    & > :last-of-type {
+      display: none;
+    }
+  }
+`;
+
+const NavItem = styled.li`
+  margin: 0;
+  padding: 0;
+`;
+
+// StyledNavLink Wraps around text+icon, meaning the redirection works on both and gap between them
+const StyledNavLink = styled(NavLink)`
+  margin: 0;
+  padding: 0;
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  gap: 20px;
+  outline: none;
+  &:focus,
+  &:hover {
+    ${Icon} {
+      fill: var(--nav-color-active);
+      transition: fill 150ms, box-shadow 150ms;
+      box-shadow: 0px 3px 10px rgba(74, 86, 226, 0.5);
+    }
+  }
+  &:focus {
+    span {
+      color: var(--nav-color-active);
+      transition: color 150ms;
+    }
+  }
+  @media (min-width: ${props => props.theme.breakpoints.tablet}) {
+    gap: 20px;
+  }
+`;
+
+const NavItemText = styled.span`
+  display: none;
+  @media (min-width: ${props => props.theme.breakpoints.tablet}) {
+    display: inline;
+    color: var(--font-color-dark);
+    transition: color 150ms;
+    font-family: Poppins;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 18px;
+    &:hover {
+      color: var(--nav-color-active);
+      transition: color 150ms;
+    }
+  }
+`;
+
+const NavigationBase = () => {
   return (
-    <>
-      <StyledDiv>
-        <NavigationElement />
-      </StyledDiv>
-    </>
+    <NavContainer>
+      <NavList>
+        <NavItem>
+          <StyledNavLink to={`/`}>
+            <Icon icon="icon__baseline-home" />
+            <NavItemText>Home</NavItemText>
+          </StyledNavLink>
+        </NavItem>
+
+        <NavItem>
+          <StyledNavLink to={`/statistic`}>
+            <Icon icon="icon__baseline-timeline" />
+            <NavItemText>Statistics</NavItemText>
+          </StyledNavLink>
+        </NavItem>
+
+        <NavItem>
+          <StyledNavLink to={`/currency`}>
+            <Icon icon="icon__baseline-dolar" />
+          </StyledNavLink>
+        </NavItem>
+      </NavList>
+    </NavContainer>
   );
 };
 
+const Navigation = styled(NavigationBase)``;
 export default Navigation;
