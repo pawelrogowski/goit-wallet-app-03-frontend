8d6728f5e704e22ef0c1e15c0e455ec6bcc0ab3c
Dawid <109389692+DawidBartus@users.noreply.github.com>
2023-07-23 17:17:01 +0200

    balance fix

--- src/components/AsideBar/AsideBar.jsx
+++ src/components/AsideBar/AsideBar.jsx
@@ -3,6 +3,7 @@ import Balance from 'components/Balance/Balance';
 import Currency from 'components/Currency/Currency';
 import Navigation from 'components/Navigation/Navigation';
 import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 const AsideContainer = styled.aside`
   margin: 0;
@@ -46,12 +47,26 @@ const FlexWrapper = styled.div`
 const AsideMenu = () => {
   const location = useLocation();
   const isCurrency = location.pathname === '/currency';
+  const isHome = location.pathname === '/';
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth > 767);
+
+  useEffect(() => {
+    console.log(windowWidth);
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth > 767);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [windowWidth]);
 
   return (
     <AsideContainer>
       <FlexWrapper>
         <Navigation />
-        <Balance />
+        {windowWidth || isHome ? <Balance /> : ''}
       </FlexWrapper>
       {isCurrency ? '' : <Currency />}
     </AsideContainer>
