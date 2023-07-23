8d0b82aaf05c6bbbfb39d1cd019756d2a49abcf6
gash94 <104354207+gash94@users.noreply.github.com>
2023-07-23 20:32:23 +0200

    add Tranasction

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,4 +1,21 @@
+import { Heading } from 'components/Heading/Heading';
+import Transactions from 'components/Transactions/Transactions';
+import styled from 'styled-components';
+
+const Title = styled(Heading)`
+  display: none;
+  padding: 0;
+  margin-bottom: 0;
+  width: 100%; // this 100% width is so the heading takes full width of a parent, pushing other elements below
+`;
+
 const DashboardPage = () => {
-  return <h1>DashBoard PAGE PAGE</h1>;
+  return (
+    <>
+      <Title as="h1">DashboardPage</Title>
+      <Transactions />
+    </>
+  );
 };
+
 export default DashboardPage;
