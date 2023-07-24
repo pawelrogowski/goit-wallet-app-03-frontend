8be4ec761ef2b98268b06c32b0a18ad4ca605bd9
Dominik Danielewicz <d.danielewicz00@gmail.com>
2023-07-24 19:47:39 +0200

    Transaction table scroll

--- src/components/Transactions/Transactions.jsx
+++ src/components/Transactions/Transactions.jsx
@@ -4,6 +4,42 @@ import { PrimaryButton } from 'components/Buttons/Buttons';
 import { data } from './data';
 import { headers } from './data';
 
+const TransactionContainer = styled.div`
+  height: 450px;
+  overflow-y: auto;
+  width: 100%;
+  &::-webkit-scrollbar {
+    width: 4px;
+  }
+  /* Track */
+  &::-webkit-scrollbar-track {
+    background: transparent;
+  }
+  /* Handle */
+  &::-webkit-scrollbar-thumb {
+    background: var(--color-logout-button);
+  }
+  /* Handle on hover */
+  &::-webkit-scrollbar-thumb:hover {
+    background: var(--color-logout-button);
+  }
+
+  mask-image: linear-gradient(to top, transparent, black),
+    linear-gradient(to left, transparent 4px, black 4px);
+  mask-size: 100% 20000px;
+  mask-position: left bottom;
+  -webkit-mask-image: linear-gradient(to top, transparent, black),
+    linear-gradient(to left, transparent 4px, black 4px);
+  -webkit-mask-size: 100% 20000px;
+  -webkit-mask-position: left bottom;
+  transition: mask-position 0.3s, -webkit-mask-position 0.3s;
+
+  &:hover {
+    -webkit-mask-position: left top;
+    mask-position: left top;
+  }
+`;
+
 const TransactionsTable = styled.table`
   border-collapse: collapse;
   border-spacing: 0;
@@ -15,11 +51,16 @@ const TransactionsTable = styled.table`
   font-style: normal;
   font-weight: 400;
   line-height: normal;
+
+  width: 100%;
 `;
 
 const TransactionsTableHead = styled.thead`
   font-size: 18px;
   font-weight: 700;
+  position: sticky;
+  top: 0;
+  z-index: 1000;
 `;
 
 const TransactionsTableHeadRow = styled.tr``;
@@ -90,6 +131,7 @@ const EditButton = styled.button`
     transform: translate(-50%, -50%);
     width: 14px;
     height: 14px;
+    z-index: 10;
   }
 `;
 
@@ -106,40 +148,42 @@ const TransactionsBodyHeadRow = styled.tr``;
 
 const Transactions = () => {
   return (
-    <TransactionsTable>
-      <TransactionsTableHead>
-        <TransactionsTableHeadRow>
-          {headers.map((item, index) => (
-            <TransactionsTableHeader key={index}>{item}</TransactionsTableHeader>
+    <TransactionContainer>
+      <TransactionsTable>
+        <TransactionsTableHead>
+          <TransactionsTableHeadRow>
+            {headers.map((item, index) => (
+              <TransactionsTableHeader key={index}>{item}</TransactionsTableHeader>
+            ))}
+          </TransactionsTableHeadRow>
+        </TransactionsTableHead>
+        <TransactionsTableBody>
+          {data.map((item, index) => (
+            <TransactionsBodyHeadRow key={index}>
+              <TransactionTableData>{item.date}</TransactionTableData>
+              <TransactionTableData>{item.type}</TransactionTableData>
+              <TransactionTableData>{item.category}</TransactionTableData>
+              <TransactionTableData>{item.comment}</TransactionTableData>
+              <TransactionTableData
+                style={{
+                  color: `${
+                    item.type === '-' ? 'var(--color-brand-accent)' : 'var(--color-brand-secondary)'
+                  }`,
+                }}
+              >
+                {item.sum}
+              </TransactionTableData>
+              <TransactionTableData>
+                <EditButton>
+                  <Icon icon="icon__edit" />
+                </EditButton>{' '}
+                <SmallButton>Delete</SmallButton>
+              </TransactionTableData>
+            </TransactionsBodyHeadRow>
           ))}
-        </TransactionsTableHeadRow>
-      </TransactionsTableHead>
-      <TransactionsTableBody>
-        {data.map((item, index) => (
-          <TransactionsBodyHeadRow key={index}>
-            <TransactionTableData>{item.date}</TransactionTableData>
-            <TransactionTableData>{item.type}</TransactionTableData>
-            <TransactionTableData>{item.category}</TransactionTableData>
-            <TransactionTableData>{item.comment}</TransactionTableData>
-            <TransactionTableData
-              style={{
-                color: `${
-                  item.type === '-' ? 'var(--color-brand-accent)' : 'var(--color-brand-secondary)'
-                }`,
-              }}
-            >
-              {item.sum}
-            </TransactionTableData>
-            <TransactionTableData>
-              <EditButton>
-                <Icon icon="icon__edit" />
-              </EditButton>{' '}
-              <SmallButton>Delete</SmallButton>
-            </TransactionTableData>
-          </TransactionsBodyHeadRow>
-        ))}
-      </TransactionsTableBody>
-    </TransactionsTable>
+        </TransactionsTableBody>
+      </TransactionsTable>
+    </TransactionContainer>
   );
 };
 
