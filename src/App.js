f2bdb507340d78d793cdd9c692e6b40c2bc36f77
Feiron <pawel.rogowski.praca@gmail.com>
2023-07-24 02:26:30 +0200

    integrated HOC redirection for auth, added auto redirect for non existing routes, fixed routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import { lazy } from 'react';
 import Layout from 'pages/Layout';
+import WithAuthRedirect from './routes/WithAuthRedirect';
+
 const Dashboard = lazy(() => import('pages/DashboardPage'));
 const Register = lazy(() => import('pages/RegistrationPage'));
 const Login = lazy(() => import('pages/LoginPage'));
@@ -10,13 +12,16 @@ const Currency = lazy(() => import('pages/CurrencyPage'));
 const App = () => {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route element={<Layout />}>
+      <Route element={<WithAuthRedirect children={<Layout />} />}>
         <Route index element={<Dashboard />} />
-        <Route path="/statistic" element={<Statistic />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/statistics" element={<Statistic />} />
         <Route path="/currency" element={<Currency />} />
       </Route>
+      <Route path="*" element={<Navigate to="/login" />} />
     </Routes>
   );
 };
