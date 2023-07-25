d6825c9758d6ec80bc114974aae2c3ace26df761
Feiron <pawel.rogowski.praca@gmail.com>
2023-07-26 01:31:17 +0200

    added global store for Loader and Modals

---
+++ src/redux/slices/globalSlice.js
@@ -0,0 +1,39 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const globalSlice = createSlice({
+  name: 'global',
+  initialState: {
+    isLoading: false, // this one is for displaying global loader
+    isModalLogoutOpen: false,
+    isModalAddTransactionOpen: false,
+    isModalEditTransactionOpen: false,
+  },
+  reducers: {
+    setIsLoading: (state, action) => {
+      state.isLoading = action.payload;
+    },
+    setIsModalLogoutOpen: (state, action) => {
+      state.isModalLogoutOpen = action.payload;
+    },
+    setIsModalAddTransactionOpen: (state, action) => {
+      state.isModalAddTransactionOpen = action.payload;
+    },
+    setIsModalEditTransactionOpen: (state, action) => {
+      state.isModalEditTransactionOpen = action.payload;
+    },
+  },
+});
+
+export const {
+  setIsLoading,
+  setIsModalLogoutOpen,
+  setIsModalAddTransactionOpen,
+  setIsModalEditTransactionOpen,
+} = globalSlice.actions;
+
+export default globalSlice.reducer;
+
+// The basic logic for modals is to dispatch an action to change the state, e.g., dispatch(setIsModalLogoutOpen(true)),
+// and conditionally render the modal or nothing based on the state flag, e.g., {isModalLogoutOpen && <LogoutModal />}
+// or {isModalLogoutOpen ? <LogoutModal /> : null}.
+// using portals would be nice too, but thats a thing for later maybve
