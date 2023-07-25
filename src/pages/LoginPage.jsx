6948232ad5dfba94df0be0901f4fcac35f75ea91
gash94 <104354207+gash94@users.noreply.github.com>
2023-07-25 18:55:27 +0200

    fix svg big logo man

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import LoginForm from 'components/LoginForm/LoginForm';
 import { Icon } from 'components/Icon/Icon';
 import styled from 'styled-components';
@@ -98,11 +99,26 @@ const LoginFormContainer = styled.div`
 `;
 
 const LoginPage = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth > 767);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth > 1279);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [windowWidth]);
   return (
     <>
       <LoginPageContainer>
         <LoginHeaderContainer>
-          <Icon className="loginIcon" icon="icon__big-logo-man1" />
+          {windowWidth ? (
+            <Icon className="loginIcon" icon="icon__big-logo-man1" />
+          ) : (
+            <Icon className="loginIcon" icon="icon__big-logo-man2" />
+          )}
           <Heading as="h1">Finance App</Heading>
         </LoginHeaderContainer>
         <LoginFormContainer>
