61dee4d2c3b7f62d883845c4c42219da9ee549d5
Feiron <pawel.rogowski.praca@gmail.com>
2023-07-25 04:42:15 +0200

    added store, usersSlice and TestLogin.jsx

--- src/index.js
+++ src/index.js
@@ -9,17 +9,20 @@ import { theme } from './stylesheet/theme';
 import { ThemeProvider } from 'styled-components';
 import { BrowserRouter } from 'react-router-dom';
 import { Background } from 'components/Background/Background';
-
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        {/* Background has fixed position, it stays in the viewport with z-index to be at
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+          {/* Background has fixed position, it stays in the viewport with z-index to be at
         the bottom, no need to wrap the App */}
-        <Background />
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+          <Background />
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
