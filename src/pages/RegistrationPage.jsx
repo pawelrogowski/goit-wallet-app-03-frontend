c5de2c658b99355b05cc90b3b454e761e2c050cd
gash94 <104354207+gash94@users.noreply.github.com>
2023-07-25 18:55:37 +0200

    fix svg big logo woman

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import RegistrationForm from 'components/RegistrationForm/RegistrationForm';
 import { Icon } from 'components/Icon/Icon';
 import styled from 'styled-components';
@@ -98,10 +99,26 @@ const RegistrationFormContainer = styled.div`
 `;
 
 const RegistrationPage = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth > 767);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth > 1279);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [windowWidth]);
   return (
     <RegistrationPageContainer>
       <RegistrationHeaderContainer>
-        <Icon className="loginIcon" icon="icon__big-logo-man1" />
+        {windowWidth ? (
+          <Icon className="loginIcon" icon="icon__big-logo-woman1" />
+        ) : (
+          <Icon className="loginIcon" icon="icon__big-logo-woman2" />
+        )}
+
         <Heading as="h1">Finance App</Heading>
       </RegistrationHeaderContainer>
       <RegistrationFormContainer>
