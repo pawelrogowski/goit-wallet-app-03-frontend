df2741c5efba076470e6ba42b33eda7ba8bb788a
gash94 <104354207+gash94@users.noreply.github.com>
2023-07-26 17:09:33 +0200

    add container

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,19 +1,8 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  // mobile styles + everything that does not change with width
-  margin: 0 auto;
-
-  // tablet styles + everything that tablet and desktop have in common
-  @media (min-width: ${props => props.theme.breakpoints.tablet}) {
-    padding: 0 117px 0 116px;
-  }
-
   // only desktop styles
   @media (min-width: ${props => props.theme.breakpoints.desktop}) {
-    max-width: 90vw;
-    padding: 0 91px 0 76px;
+    max-width: 1400px;
   }
 `;
-
-export const ContainerWallet = styled(Container)``;
