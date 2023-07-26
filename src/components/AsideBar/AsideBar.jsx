80eb28358ae4c79dfecde83cd5d01866b7570448
Dominik Danielewicz <d.danielewicz00@gmail.com>
2023-07-26 16:49:37 +0200

    Layout changes

--- src/components/AsideBar/AsideBar.jsx
+++ src/components/AsideBar/AsideBar.jsx
@@ -12,6 +12,8 @@ const AsideContainer = styled.aside`
   max-width: 462px;
   display: flex;
   flex-direction: column;
+  padding: 14px 20px 0px 20px;
+
   @media (max-width: ${props => props.theme.breakpoints.desktopForMaxMedia}) {
     flex-grow: 0;
     flex-direction: row;
@@ -21,9 +23,15 @@ const AsideContainer = styled.aside`
     max-width: none;
     gap: 32px;
   }
+
+  @media (min-width: ${props => props.theme.breakpoints.tablet}) {
+    padding: 32px 32px 0px 32px;
+  }
+
   @media (min-width: ${props => props.theme.breakpoints.desktopForMaxMedia}) {
     border-right: 2px solid #e7e5f2;
     box-shadow: 1px 0 0 0 rgba(255, 255, 255, 0.6);
+    padding: 40px 69px 20px 16px;
   }
 `;
 
@@ -35,14 +43,21 @@ const FlexWrapper = styled.div`
   justify-content: flex-end;
   height: 100%;
   gap: 28px;
+
   @media (max-width: ${props => props.theme.breakpoints.tabletForMaxMedia}) {
     align-items: center;
     margin-top: 15px;
     gap: 12px;
   }
+
+  @media (max-width: ${props => props.theme.breakpoints.tabletForMaxMedia}) {
+    align-items: center;
+    margin-top: 15px;
+    gap: 12px;
+  }
+
   @media (min-width: ${props => props.theme.breakpoints.desktopForMaxMedia}) {
     height: auto;
-    margin-top: 40px;
   }
 `;
 
