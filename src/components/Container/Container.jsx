7aa397e0c0a1b15b02530f5627d251a08264080b
gash94 <104354207+gash94@users.noreply.github.com>
2023-07-26 17:20:40 +0200

    fix container

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   // only desktop styles
+  margin: 0 auto;
   @media (min-width: ${props => props.theme.breakpoints.desktop}) {
     max-width: 1400px;
   }
