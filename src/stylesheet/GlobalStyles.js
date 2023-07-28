8d8510f3027ba137cc3db67a39e43c0f3c4aeffe
Feiron <pawel.rogowski.praca@gmail.com>
2023-07-28 22:56:02 +0200

    scoped the active class to nav

--- src/stylesheet/GlobalStyles.js
+++ src/stylesheet/GlobalStyles.js
@@ -2,22 +2,22 @@ import { createGlobalStyle } from 'styled-components';
 import { Icon } from 'components/Icon/Icon';
 export const GlobalStyles = createGlobalStyle`
 
-
- .active{
-    ${Icon} {
-      fill: var(--nav-color-active);
-      transition: fill 150ms, box-shadow 150ms;
-      box-shadow: 0px 3px 10px rgba(74, 86, 226, 0.5);
-    }
-    span {
-      color: var(--font-color-dark);
-      font-size: 18px;
-      font-weight: 700;
-      font-style: normal;
-      &:hover {
-        color: var(--nav-color-active);
+  nav {
+    .active{
+        ${Icon} {
+          fill: var(--nav-color-active);
+          transition: fill 150ms, box-shadow 150ms;
+          box-shadow: 0px 3px 10px rgba(74, 86, 226, 0.5);
+        }
+        span {
+          color: var(--font-color-dark);
+          font-size: 18px;
+          font-weight: 700;
+          font-style: normal;
+          &:hover {
+            color: var(--nav-color-active);
+          }
+        }
       }
     }
-  }
-
 `;
