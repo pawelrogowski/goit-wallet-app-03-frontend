a318bb6556a3fa4f496cbf11effc3839d44b4422
Feiron <pawel.rogowski.praca@gmail.com>
2023-07-29 20:03:08 +0200

    added more number utils and used in balance

--- src/utils/numberUtils.js
+++ src/utils/numberUtils.js
@@ -5,3 +5,11 @@ export const fixDigitsToTwoDecimalPlaces = number => {
 
   return parseFloat(number.toFixed(2));
 };
+
+export const formatNumberWithSpaces = number => {
+  if (typeof number !== 'number') {
+    throw new Error('Input must be a number');
+  }
+
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+};
