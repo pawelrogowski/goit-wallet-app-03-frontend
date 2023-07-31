55092ed6b8c047096ac6e9cc9589ad34ce6e0ae0
gash94 <104354207+gash94@users.noreply.github.com>
2023-07-31 12:36:07 +0200

    Delete Container.jsx

--- src/components/Container/Container.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-  // only desktop styles
-  margin: 0 auto;
-  @media (min-width: ${props => props.theme.breakpoints.desktop}) {
-    max-width: 1400px;
-  }
-`;
