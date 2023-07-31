a278865a4ed5919660ff5f68d292905dad353228
gash94 <104354207+gash94@users.noreply.github.com>
2023-07-31 21:56:28 +0200

    refactor addTransactionModal

--- src/components/AddTransactionModal/AddTransactionModal.jsx
+++ src/components/AddTransactionModal/AddTransactionModal.jsx
@@ -1,13 +1,11 @@
-import styled from 'styled-components';
 import { Icon } from 'components/Icon/Icon';
 import Switch from './../Switch/Switch';
-import { Formik, Form, ErrorMessage } from 'formik';
+import { Formik } from 'formik';
 import { object, string, date, bool, mixed, number } from 'yup';
-import { PrimaryButton, SecondaryButton } from '../Buttons/Buttons';
-import { BaseInput } from 'components/Inputs/BaseInput';
+import { PrimaryButton } from '../Buttons/Buttons';
+import { BaseInput } from 'components/Inputs/BaseInput.styled';
 import { useEffect, useState } from 'react';
 import CategorySelect from 'components/CategorySelect/CategorySelect';
-import { options } from 'components/AddTransactionModal/data';
 import DatetimePicker from 'components/DatetimePicker/DatetimePicker';
 import { formatDate } from 'utils/formaters';
 import { dateTransformer } from 'utils/formaters';
@@ -15,179 +13,30 @@ import { useDispatch } from 'react-redux';
 import { setIsModalAddTransactionOpen } from 'redux/slices/globalSlice';
 import { addTransaction, fetchTransactions } from 'redux/slices/transactionSlice';
 import Textarea from 'components/Inputs/Textarea';
+import {
+  Backdrop,
+  CalendarWrapper,
+  CancelButton,
+  CloseButton,
+  ErrorText,
+  FormikForm,
+  Heading,
+  InputWrapper,
+  Modal,
+  TwoColumnRow,
+} from './AddTransactionModal.styled';
 
-const Backdrop = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: fixed;
-  top: 0;
-  right: 0;
-  height: 100vh;
-  left: 0;
-  background-color: var(--background-overlay-modal);
-  transition: opacity 500ms ease-in-out, visibility 500ms ease-in-out;
-  opacity: 1;
-  visibility: visible;
-  z-index: 1000;
-
-  .category {
-    top: 44px;
-  }
-
-  @media (min-width: ${props => props.theme.breakpoints.tabletForMaxMedia}) {
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-  }
-`;
-
-const Modal = styled.div`
-  min-width: 540px;
-  padding: 40px 73px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  background: var(--background-light);
-  border-radius: 12px;
-  position: relative;
-
-  @media (max-width: ${props => props.theme.breakpoints.tabletForMaxMedia}) {
-    width: 100%;
-    height: 100%;
-    border-radius: 0px;
-    padding: 20px;
-    min-width: 300px;
-  }
-`;
-
-const Heading = styled.h3`
-  color: var(--font-color-dark);
-  text-align: center;
-  font-family: Poppins;
-  font-size: 30px;
-  font-weight: 400;
-  margin: 0;
-`;
-
-const CloseButton = styled.button`
-  position: absolute;
-  top: 20px;
-  right: 20px;
-  width: 20px;
-  height: 20px;
-  border: none;
-  background: none;
-  margin: 0;
-  padding: 0;
-  cursor: pointer;
-
-  & svg {
-    stroke: var(--font-color-dark);
-    width: 20px;
-    height: 20px;
-    position: relative;
-  }
-
-  @media (max-width: ${props => props.theme.breakpoints.tabletForMaxMedia}) {
-    & svg {
-      display: none;
-    }
-  }
-`;
-
-const FormikForm = styled(Form)`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  row-gap: 40px;
-`;
-
-const CancelButton = styled(SecondaryButton)`
-  margin-bottom: 0;
-  margin-top: -40px;
-`;
-
-const TwoColumnRow = styled.div`
-  display: flex;
-  flex-direction: row;
-  gap: 41px;
-  max-width: 410px;
-
-  input {
-    width: 181px;
-  }
-
-  & > :first-child input {
-    font-family: Circe;
-    font-size: 18px;
-    font-weight: 700;
-    text-align: center;
-    margin-bottom: 3px;
-    outline: none;
-
-    @media (max-width: ${props => props.theme.breakpoints.tabletForMaxMedia}) {
-      text-align: left;
-    }
-  }
-
-  @media (max-width: ${props => props.theme.breakpoints.tabletForMaxMedia}) {
-    flex-direction: column;
-    max-width: 100%;
-    width: 100%;
-
-    input {
-      width: 100%;
-    }
-  }
-`;
-
-const InputWrapper = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  gap: 2px;
-  width: 100%;
-
-  ${BaseInput} {
-    @media (max-width: ${props => props.theme.breakpoints.tabletForMaxMedia}) {
-      padding-left: 20px;
-      text-align: left;
-    }
-  }
-`;
-
-const CalendarWrapper = styled.div`
-  position: relative;
-  margin-top: 2px;
-
-  svg {
-    position: absolute;
-    bottom: 6px;
-    right: 0;
-    width: 24px;
-    height: 24px;
-    fill: var(--color-brand-primary);
-  }
-
-  @media (max-width: ${props => props.theme.breakpoints.tabletForMaxMedia}) {
-    svg {
-      right: 20px;
-      bottom: 2px;
-    }
-  }
-`;
-
-const ErrorText = styled(ErrorMessage)`
-  position: absolute;
-  top: 28px;
-  left: 0px;
-  font-size: 13px;
-  color: var(--color-brand-accent);
-`;
+const options = [
+  { value: 'main expenses', label: 'Main expenses' },
+  { value: 'products', label: 'Products' },
+  { value: 'self care', label: 'Self care' },
+  { value: 'child care', label: 'Child care' },
+  { value: 'household products', label: 'Household products' },
+  { value: 'education', label: 'Education' },
+  { value: 'leisure', label: 'Leisure' },
+  { value: 'other expenses', label: 'Other expenses' },
+  { value: 'entertainment', label: 'Entertainment' },
+];
 
 const AddTransactionModal = () => {
   const [isChecked, setIsChecked] = useState(false);
