29d8b6f4bd8b9997a76a19c6b14adbc8003873d9
gash94 <104354207+gash94@users.noreply.github.com>
2023-07-31 21:54:31 +0200

    refactor currency

--- src/components/Currency/Currency.jsx
+++ src/components/Currency/Currency.jsx
@@ -1,79 +1,13 @@
 import { useEffect } from 'react';
-import styled from 'styled-components';
-import triangles from './currencyBackground.svg';
+import triangles from '../../assets/icons/currencyBackground.svg';
 import getCurrencyData from './CurrencyFetchData';
 import { useLocation, useNavigate } from 'react-router-dom';
-
-const TableStyledContainer = styled.ul`
-  list-style: none;
-  margin: 0;
-  padding: 0px;
-  width: 100%;
-  max-width: 393px;
-  background-color: var(--color-brand-primary);
-  border-radius: 30px;
-  overflow: hidden;
-  position: relative;
-  overflow-y: auto;
-  height: 100%;
-  min-height: 333px;
-  background-repeat: no-repeat;
-  background-position: bottom;
-  background-size: contain;
-  li:first-child {
-    background-color: #6e78e8;
-    margin-bottom: 16px;
-    position: sticky;
-    top: 0;
-  }
-  li:last-of-type {
-    margin-bottom: 20px;
-  }
-  @media (min-width: ${props => props.theme.breakpoints.tabletForMaxMedia}) {
-    min-height: 182px;
-    max-height: 331px;
-  }
-  @media (max-width: ${props => props.theme.breakpoints.desktopForMaxMedia}) {
-    max-height: 182px;
-    max-width: 333px;
-  }
-  @media (max-width: ${props => props.theme.breakpoints.tabletForMaxMedia}) {
-    display: none;
-  }
-
-  &::-webkit-scrollbar {
-    display: none;
-  }
-`;
-
-const StyledHeaderParagraph = styled.p`
-  margin: 0;
-  color: var(--background-light);
-  font-family: 'Circe';
-  font-size: 18px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: normal;
-  padding: 11px 0px 12px 0px;
-  flex-basis: 33.33%;
-`;
-const StyledBodyParagraph = styled.p`
-  margin: 0;
-  color: var(--background-light);
-  font-family: 'Circe';
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: normal;
-  padding-bottom: 12px;
-  flex-basis: 33.33%;
-`;
-
-const StyledListElement = styled.li`
-  display: flex;
-  justify-content: space-around;
-  text-align: center;
-`;
+import {
+  StyledBodyParagraph,
+  StyledHeaderParagraph,
+  StyledListElement,
+  TableStyledContainer,
+} from './Currency.styled';
 
 const currencyData = await getCurrencyData();
 const Currency = () => {
