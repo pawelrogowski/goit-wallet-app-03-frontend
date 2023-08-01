f748c33b258b862bbfd6248bc01d4a9b9dfb9315
Feiron <pawel.rogowski.praca@gmail.com>
2023-08-01 18:10:27 +0200

    modified:   src/utils/lockScreen.js

--- src/utils/lockScreen.js
+++ src/utils/lockScreen.js
@@ -1,1 +1,8 @@
-window.screen.orientation.lock('portrait');
+function lockScreenOrientation() {
+  if (window.screen.orientation && window.screen.orientation.lock) {
+    window.screen.orientation.lock('portrait');
+  }
+}
+document.addEventListener('DOMContentLoaded', function () {
+  lockScreenOrientation();
+});
