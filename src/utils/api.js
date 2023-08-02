0d8e3a191c19cd95ee2254046e546072d961660f
Feiron <pawel.rogowski.praca@gmail.com>
2023-08-02 02:21:37 +0200

    modified:   src/utils/api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -15,7 +15,7 @@ WalletInstance.interceptors.response.use(
   async error => {
     const originalRequest = error.config;
 
-    if (error.response.status === 401 && !originalRequest._retry) {
+    if (error.response && error.response.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
 
       try {
